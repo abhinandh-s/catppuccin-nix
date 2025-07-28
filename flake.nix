6cf67679afdb6795883c40205190dac1ddae1072
abhi <abhinandhsuby@proton.me>
2025-07-28 19:51:15 +0530

    feat: rbga with spot declaration

--- flake.nix
+++ flake.nix
@@ -1,33 +1,50 @@
 {
   description = "A flake for Catppuccin color palette";
 
-  inputs = { };
+  inputs = {};
 
-  outputs = { self }: {
+  outputs = {self}: let
+    rgb_attr = import ./palette/rgb_attr.nix;
+    toRgba = color: alpha: "rgba(${toString color.r},${toString color.g},${toString color.b},${toString alpha})";
+    toRgb = color: "rgb(${toString color.r},${toString color.g},${toString color.b})";
+  in {
     catppuccin = {
+      bare = import ./palette/bare.nix;
+      hex = import ./palette/hex.nix;
+      hsl = import ./palette/hsl.nix;
       latte = self.catppuccin.hex.latte;
       frappe = self.catppuccin.hex.frappe;
       macchiato = self.catppuccin.hex.macchiato;
       mocha = self.catppuccin.hex.mocha;
-      bare = import ./palette/bare.nix;
-      hex = import ./palette/hex.nix;
-      hsl = import ./palette/hsl.nix;
-      rgb = import ./palette/rgb.nix;
-      rgba = import ./palette/rgba.nix;
     };
 
-    overlays.default = final: prev: {
-      catppuccin = self.catppuccin;
+    overlays.default = final: prev: let
+      rgbaify = colors: prev.lib.mapAttrs (_name: color: alpha: toRgba color alpha) colors;
+      rgbify = colors: prev.lib.mapAttrs (_name: color: toRgb color) colors;
+    in {
+      catppuccin = {
+        inherit (self.catppuccin) latte frappe macchiato mocha bare hex hsl;
+        rgb = prev.lib.mapAttrs (_flavor: palette: rgbify palette) rgb_attr;
+        rgba = prev.lib.mapAttrs (_flavor: palette: rgbaify palette) rgb_attr;
+      };
     };
 
-    homeManagerModules.default = { lib, ... }: {
+    homeManagerModules.default = {lib, ...}: let
+      rgbify = colors: lib.mapAttrs (_name: color: toRgb color) colors;
+      rgaify = colors: lib.mapAttrs (_name: color: alpha: toRgba color alpha) colors;
+      default = {
+        inherit (self.catppuccin) latte frappe macchiato mocha bare hex hsl;
+        rgb = lib.mapAttrs (_flavor: palette: rgbify palette) rgb_attr;
+        rgba = lib.mapAttrs (_flavor: palette: rgaify palette) rgb_attr;
+      };
+    in {
       options.catppuccin = lib.mkOption {
         type = lib.types.attrs;
         readOnly = true;
-        default = self.catppuccin;
+        inherit default;
         description = "Catppuccin color palette";
       };
       config = {};
-   };
+    };
   };
 }
